bananadroid/android_packages_apps_Launcher3

Launcher3: Update settings style

src/com/android/launcher3/settings/SettingsActivity.java
@@ -128,7 +128,7 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
     public boolean onPreferenceStartScreen(PreferenceFragmentCompat caller, PreferenceScreen pref) {
         Bundle args = new Bundle();
         args.putString(PreferenceFragmentCompat.ARG_PREFERENCE_ROOT, pref.getKey());
-        return startPreference(getString(R.string.settings_title), args, pref.getKey());
+        return startPreference(getString(R.string.custom_app_name), args, pref.getKey());
     }
 
     @Override

NAAP-again/packages_apps_Launcher3

src/com/android/launcher3/settings/SettingsActivity.java
@@ -127,7 +127,7 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
     public boolean onPreferenceStartScreen(PreferenceFragmentCompat caller, PreferenceScreen pref) {
         Bundle args = new Bundle();
         args.putString(ARG_PREFERENCE_ROOT, pref.getKey());
-        return startPreference(getString(R.string.settings_title), args, pref.getKey());
+        return startPreference(getString(R.string.everest_app_name), args, pref.getKey());
     }
 
     @Override